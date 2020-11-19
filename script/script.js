bdab2245517f11bcccc6d8dae78b6b68f0e20aab
HoussamMrabet <dodslim100@gmail.com>
2020-11-19 21:43:23 +0100

    timer script

--- script/script.js
+++ script/script.js
@@ -11,9 +11,32 @@ function blue() {
 }
 
 function pink() {
-    document.getElementById("myimage").src = "https://i.imgur.com/Zygu7I3.png";
+    document.getElementById("myimage").src = "https://i.imgur.com/xSIK4M8.png";
 }
 
 function purple() {
-    document.getElementById("myimage").src = "https://i.imgur.com/xSIK4M8.png";
+    document.getElementById("myimage").src = "https://i.imgur.com/Zygu7I3.png";
 }
+
+function timer() {
+
+    function addZero(i) {
+        if (i < 10) {
+          i = "0" + i;
+        }
+        return i;
+    }
+    
+    setInterval(function(){
+        var d = new Date();
+        var x = document.getElementById("demo");
+        var h = addZero(d.getHours());
+        var m = addZero(d.getMinutes());
+        var s = addZero(d.getSeconds());
+        document.getElementById('time').innerHTML = h + " : " + m + " : " + s;
+    },1000);
+}
+
+
+
+
