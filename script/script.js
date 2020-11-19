7f4378d1a429d87ed220bf23d1a32eb8d7bf5954
HoussamMrabet <dodslim100@gmail.com>
2020-11-19 21:05:56 +0100

    select colors script

--- script/script.js
+++ script/script.js
@@ -1,1 +1,19 @@
-document.getElementById
+function black() {
+    document.getElementById("myimage").src = "https://i.imgur.com/iOeUBV7.png";
+}
+
+function red() {
+    document.getElementById("myimage").src = "https://i.imgur.com/PTgQlim.png";
+}
+
+function blue() {
+    document.getElementById("myimage").src = "https://i.imgur.com/Mplj1YR.png";
+}
+
+function pink() {
+    document.getElementById("myimage").src = "https://i.imgur.com/Zygu7I3.png";
+}
+
+function purple() {
+    document.getElementById("myimage").src = "https://i.imgur.com/xSIK4M8.png";
+}
