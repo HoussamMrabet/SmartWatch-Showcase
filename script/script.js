a7d413256534b4a04225edda398282c0b269ba67
HoussamMrabet <dodslim100@gmail.com>
2020-11-19 22:32:06 +0100

    features options select script, finishing the project

--- script/script.js
+++ script/script.js
@@ -20,21 +20,35 @@ function purple() {
 
 function timer() {
 
-    function addZero(i) {
-        if (i < 10) {
-          i = "0" + i;
-        }
-        return i;
-    }
+    document.getElementById('heart').style.visibility = 'hidden';
+    document.getElementById('heart-rate').style.visibility = 'hidden';
+    document.getElementById('time').style.visibility = 'visible';
+
     
     setInterval(function(){
+
+        function addZero(i) {
+            if (i < 10) {
+              i = "0" + i;
+            }
+            return i;
+        }
+    
         var d = new Date();
         var x = document.getElementById("demo");
         var h = addZero(d.getHours());
         var m = addZero(d.getMinutes());
         var s = addZero(d.getSeconds());
         document.getElementById('time').innerHTML = h + " : " + m + " : " + s;
-    },1000);
+    },0);
+}
+
+function heartbeat() {
+
+    document.getElementById('time').style.visibility = 'hidden';
+    document.getElementById('heart').style.visibility = 'visible';
+    document.getElementById('heart-rate').style.visibility = 'visible';
+
 }
 
 
